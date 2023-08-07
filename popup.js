f57ea25afa2797ee9a931c8a0bc55a203cb93ef0
Kory Smith <kor54e@gmail.com>
2023-08-07 10:54:00 -0400

    triggers the notification via a button and includes the current URL in the notification

--- popup.js
+++ popup.js
@@ -1,10 +1,18 @@
-window.onload = function() {
-	setTimeout(() => {
-			chrome.notifications.create({
-					iconUrl: "images/notification-24.png",
-					type: 'basic',
-					title: 'Hello',
-					message: 'World!'
+document.addEventListener('DOMContentLoaded', function() {
+	const notifyButton = document.getElementById('notifyButton');
+
+	notifyButton.addEventListener('click', function() {
+			// Query the active tab in the current window
+			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+					const currentTab = tabs[0]; // There will be only one in this array
+					const url = currentTab.url; // This is the URL of the current active tab
+
+					chrome.notifications.create({
+							iconUrl: 'images/notification-24.png',
+							type: 'basic',
+							title: 'Current URL',
+							message: url
+					});
 			});
-	}, 1000);
-};
+	});
+});
