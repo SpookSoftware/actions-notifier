b91f2dc284a842ec805ebab62aae346e44af84bf
Kory Smith <kory.smith@kroger.com>
2023-08-07 13:37:46 -0400

    adds basic notification functionality with hardcoded values and token auth

--- popup.js
+++ popup.js
@@ -1,18 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-	const notifyButton = document.getElementById('notifyButton');
-
-	notifyButton.addEventListener('click', function() {
-			// Query the active tab in the current window
-			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-					const currentTab = tabs[0]; // There will be only one in this array
-					const url = currentTab.url; // This is the URL of the current active tab
-
-					chrome.notifications.create({
-							iconUrl: 'images/notification-24.png',
-							type: 'basic',
-							title: 'Current URL',
-							message: url
-					});
-			});
-	});
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("notifyButton").addEventListener("click", () => {
+    chrome.runtime.sendMessage({ action: "startMonitoring" });
+  });
 });
