6035597d3735459de1ada079c3daa60f2edd2274
Kory Smith <kor54e@gmail.com>
2023-08-13 12:35:40 -0400

    surrounds all the debug logs with newlines so they're easy to spot

--- popup.js
+++ popup.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("notifyButton").addEventListener("click", () => {
     // chrome.runtime.sendMessage({ action: "startMonitoring" });
+
     console.debug("Creating alarm with name Kory")
+
     chrome.alarms.create("Kory", {
       periodInMinutes: .1
     })
