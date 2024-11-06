dc069b6151b1a10b39b60a1e1f68c3301af87ba5
Kory Smith <kor54e@gmail.com>
2024-11-06 16:26:25 -0500

    adds file that starts chrome in debugging mode for testing purposes

--- launch-chrome.js
+++ launch-chrome.js
@@ -1,7 +1,7 @@
 import { spawn } from 'child_process';
 import path from 'path';
 
-const CHROME_EXECUTABLE = '/users/KS61347/Applications/Google Chrome.app'; // Update this path
+const CHROME_EXECUTABLE = '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'; // Update this path
 const EXTENSION_PATH = path.resolve('./extension');
 const USER_DATA_DIR = path.resolve('./.chrome-user-data'); // Separate user data dir for development
 const REMOTE_DEBUGGING_PORT = 9222;
@@ -10,7 +10,6 @@ const chromeArgs = [
   `--load-extension=${EXTENSION_PATH}`,
   `--remote-debugging-port=${REMOTE_DEBUGGING_PORT}`,
   `--user-data-dir=${USER_DATA_DIR}`,
-  '--disable-web-security',
   '--disable-extensions-except',
   '--disable-component-extensions-with-background-pages',
 ];
