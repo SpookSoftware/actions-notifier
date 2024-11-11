bccec9032e1c90d1461266822949c1f1b4430a21
Kory Smith <kor54e@gmail.com>
2024-11-11 16:49:41 -0500

    puts the helper tests in the helper tests file!

---
+++ tests/helpers.test.ts
@@ -0,0 +1,27 @@
+import { expect, describe, it } from "bun:test";
+
+import { shouldAddActionNotificationButton } from "../extension/helpers";
+
+describe("shouldAddActionNotificationButton", () => {
+  it("should match specific workflow run URLs", () => {
+    expect(
+      shouldAddActionNotificationButton(
+        "https://github.com/SpookSoftware/github-actions-browser-notifications/actions/workflows/waitAMinute.yml"
+      )
+    ).toBeTrue();
+  });
+  it("should match all workflows URLs", () => {
+    expect(
+      shouldAddActionNotificationButton(
+        "https://github.com/SpookSoftware/github-actions-browser-notifications/actions"
+      )
+    ).toBeTrue();
+  });
+  it("should match pull request URLs", () => {
+    expect(
+      shouldAddActionNotificationButton(
+        "https://github.com/SpookSoftware/github-actions-browser-notifications/pull/22932/checks"
+      )
+    ).toBeTrue();
+  });
+});
