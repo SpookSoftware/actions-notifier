5631de92624586655abf4e61d2575cb5bef490c5
Kory Smith <kor54e@gmail.com>
2024-11-12 16:24:10 -0500

    removes all the filesystem cache stuff

--- tests/selectors.test.ts
+++ tests/selectors.test.ts
@@ -1,7 +1,5 @@
 import { expect, describe, it } from "bun:test";
-import { writeFile, mkdir, unlink, readFile, access } from "node:fs/promises";
 import pMemoize from "p-memoize";
-import filenamify from "filenamify";
 import { parseHTML } from "linkedom";
 import {
   WORKFLOW_RUN_ATTRIBUTE_SELECTOR,
@@ -13,49 +11,14 @@ import {
   PR_CHECKS_ACTION_LINK_SELECTOR,
 } from "../extension/selectors";
 
-const fsCache = {
-  async get(path: string): Promise<string | undefined> {
-    try {
-      const value = await readFile(path, "utf8");
-      return value;
-    } catch {
-      return undefined;
-    }
-  },
-  async set(path: string, contents: string): Promise<void> {
-    await mkdir("./tests/.cache", { recursive: true });
-    await writeFile(path, contents);
-  },
-  async has(path: string): Promise<boolean> {
-    try {
-      await access(path);
-      return true;
-    } catch {
-      return false;
-    }
-  },
-  async delete(path: string): Promise<void> {
-    await unlink(path);
-  },
-} as const;
-
-const fetchDocument = pMemoize(
-  async (url: string): Promise<string> => {
-    const request = await fetch(url, {
-      headers: {
-        Accept: "text/html",
-      },
-    });
-    return request.text();
-  },
-  {
-    cacheKey: ([url]) =>
-      `./tests/.cache/${filenamify(
-        url.replace("https://github.com", "")
-      )}.html`,
-    cache: fsCache,
-  }
-);
+const fetchDocument = pMemoize(async (url: string): Promise<string> => {
+  const request = await fetch(url, {
+    headers: {
+      Accept: "text/html",
+    },
+  });
+  return request.text();
+});
 
 async function getMatchesFor(url: string, selector: string) {
   const html = await fetchDocument(url);
