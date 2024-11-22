4bcfd58dd68d09b10695680b20e9bf6daefcd664
Kory Smith <kor54e@gmail.com>
2024-11-22 16:52:05 -0500

    uses the storage API to set and get the github token

--- extension/popup.js
+++ extension/popup.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("notifyButton").addEventListener("click", () => {
-    // chrome.runtime.sendMessage({ action: "startMonitoring" });
+  chrome.storage.local.get("githubToken", (data) => {
+    if (data.githubToken) {
+      document.getElementById("githubToken").value = data.githubToken;
+    }
+  });
 
-    console.debug("Creating alarm with name Kory");
-
-    chrome.alarms.create("Kory", {
-      periodInMinutes: 0.1,
+  document.getElementById("tokenForm").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const token = document.getElementById("githubToken").value;
+    chrome.storage.local.set({ githubToken: token }, () => {
+      console.debug("GitHub token saved.");
     });
   });
 });
